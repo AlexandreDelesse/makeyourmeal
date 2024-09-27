1698ed31e6f7e76a90a4e760f9b73cf6d8b020f8
Alexandre <alexandre.delesse@isen.yncrea.fr>
2024-09-27 12:54:18 +0200

    add font with back

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const ingredientRoute = require("./routes/ingredient.route");
 
 const express = require("express");
 const cors = require("cors");
+const path = require("path");
 
 const app = express();
 const PORT = 3001;
@@ -11,8 +12,13 @@ const CORS_OPTIONS = {
 
 app.use(express.json());
 app.use(cors(CORS_OPTIONS));
+app.use(express.static(path.join(__dirname, "client/build")));
 
-app.get("/", (req, res) => res.send("Hello world"));
 app.use("/ingredients", ingredientRoute);
+
+app.get("/hello", (req, res) => res.send("Hello world"));
+app.get("*", (req, res) =>
+  res.sendFile(path.join(__dirname, "client/build", "index.html"))
+);
 
 app.listen(PORT, () => console.log("App is listening on port : ", PORT));
